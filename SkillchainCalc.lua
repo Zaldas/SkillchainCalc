39dd09c392763b3a2b5812b72440b4a66992f269
Zaldas <anas_alkhatib@hotmail.com>
2025-12-18 01:35:45 -0600

    Version Bump

--- SkillchainCalc.lua
+++ SkillchainCalc.lua
@@ -3,7 +3,7 @@
 
 addon.name      = 'SkillchainCalc';
 addon.author    = 'Zalyx';
-addon.version   = '2.01';
+addon.version   = '2.1';
 addon.desc      = 'Skillchain combination calculator';
 addon.link      = 'https://github.com/Zaldas/SkillchainCalc';
 
